cc2e6898ed785d17c76c59c94b2d2dd50c20a8ea
Adrian <ar.hordyk@gmail.com>
2019-11-12 16:33:27 -0800

    update package

---
+++ R/SWOMSE.R
@@ -0,0 +1,9 @@
+#' SWOMSE: A package for conducting management strategy evaluation (MSE) for
+#' North Atlantic Swordfish
+#'
+#' Type `SWO_userguide()` for detailed instructions on using the package.
+#'
+#' @docType package
+#' @author A. Hordyk
+#' @name SWOMSE
+NULL
