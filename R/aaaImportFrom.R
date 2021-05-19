f9fc2adb01a11a1049fe513854e23946dd5b0484
Adrian <ar.hordyk@gmail.com>
2021-05-19 14:07:11 -0700

    remove old files

--- R/aaaImportFrom.R
+++
@@ -1,5 +0,0 @@
-#' @importFrom graphics abline hist legend lines par plot points matplot text segments
-#' @importFrom grDevices dev.off gray png
-#' @importFrom methods new slot
-#' @importFrom utils packageVersion
-#' @importFrom stats acf aggregate dnorm optim qlnorm qnorm quantile rnorm sd
