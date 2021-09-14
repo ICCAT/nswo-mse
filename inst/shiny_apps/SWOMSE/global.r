cf7a284970958be9f720b8ec7d850670b4fd9684
Adrian <ar.hordyk@gmail.com>
2021-09-14 16:47:49 -0700

    update shiny app

--- inst/shiny_apps/SWOMSE/global.r
+++ inst/shiny_apps/SWOMSE/global.r
@@ -2,66 +2,225 @@ library(SWOMSE)
 library(dplyr)
 
 
-# Generate Table of OM Assumptions
+# Generate Table of OM Axes of Uncertainty
 
-# Base Case
-# TODO: Check assumptions/parameters for base case
 makeDF <- function(grid.dir) {
-  DFbc <- data.frame(Num=0,
-                     Name="OM_base_case",
-                     M=unique(OM_base_case@M),
-                     sigmaR=0.2,
-                     steepness=0.88,
-                     cpuecv=0.3,
-                     ess=2,
-                     llq=1,
-                     env=-4,
-                     stringsAsFactors = FALSE
-  )
 
-  # uncertainty grid
+  save.dir <- 'inst/shiny_apps/SWOMSE/data/'
+  ss.dirs <- list.dirs(grid.dir, recursive = FALSE)
 
-  ss.dirs <- list.dirs(grid.dir)
-  count <- 0; outlist <- list()
+  # MSE files
+  dir.create(file.path(save.dir, basename(grid.dir)))
+
+  outlist <- list();
   for (i in 1:length(ss.dirs)) {
-    ss.dir <- file.path(grid.dir, ss.dirs[i])
+    message(i, '/', length(ss.dirs))
+    ss.dir <- file.path(ss.dirs[i])
 
-    tdf <- stringr::str_extract_all(basename(ss.dir),"\\(?[0-9,.]+\\)?")[[1]]
-    tdf <- matrix(as.numeric(tdf), nrow=1, ncol=9) %>% data.frame(stringsAsFactors = FALSE)
-    colnames(tdf) <- c("Num", 'M', "sigmaR", 'steepness', 'cpuecv', 'ess', 'llq', 'env', 'num2')
-    tdf <- tdf %>% select('Num', 'M', "sigmaR", 'steepness', 'cpuecv', 'ess', 'llq', 'env')
+    MSE <- readRDS(file.path(ss.dir, 'MSE.rda'))
+    MSE@PPD <- list()
+    MSE@Hist <- new('Hist')
+    MSE@CB_hist <- MSE@FM_hist <- MSE@SSB_hist <- matrix()
+    MSE@RefPoint <-  MSE@BioEco <- list()
 
-    is.neg <- stringr::str_extract(basename(ss.dir), 'env-')
-    if (!is.na(is.neg)) tdf$env <- -tdf$env
+    dir.create(file.path(save.dir, basename(grid.dir), i))
+    saveRDS(MSE, paste0(file.path(save.dir, basename(grid.dir), i), '/MSE.rda'))
 
-    OMNums <- stringr::str_extract_all(SWOM@Source, "\\(?[0-9,.]+\\)?") %>% unlist() %>%  as.numeric()
+    vals <- as.numeric(strsplit(MSE@Name, '-')[[1]][seq(2, by=2, length=6)])
 
-    if (tdf$Num %in% OMNums) {
-      tdf$Name <- paste0('OM_', tdf$Num)
-      count <- count+1
-      outlist[[count]] <- tdf
+    outlist[[i]] <- data.frame(i=i,
+                               M=vals[1],
+                               sigmaR=vals[2],
+                               h=vals[3],
+                               lambda=vals[4],
+                               incQ=vals[5],
+                               incE=vals[6])
+  }
+
+  DF <- do.call('rbind', outlist)
+  saveRDS(DF, paste0(file.path(save.dir, basename(grid.dir)), '/DF.rda'))
+
+}
+
+# # Run function to save parameter assumption table
+# # Grid Dir - Original Size Comp
+# OMgrid <- 'G:/My Drive/1_PROJECTS/North_Atlantic_Swordfish/OMs/grid_2021/grid_May2021/'
+#
+# makeDF(OMgrid)
+#
+# # Grid Dir - Adjusted Size Comp
+# OMgrid_adjusted <- 'G:/My Drive/1_PROJECTS/North_Atlantic_Swordfish/OMs/grid_2021/grid_May2021_shifted/'
+#
+# makeDF(OMgrid_adjusted)
+#
+# # Make overall OM table
+# DF1 <- readRDS('inst/shiny_apps/SWOMSE/data/grid_May2021/DF.rda')
+# DF2 <- readRDS('inst/shiny_apps/SWOMSE/data/grid_May2021_shifted/DF.rda')
+#
+# DF1$Size <- 'Original'
+# DF1$Name <- paste0('OM_',DF1$i)
+#
+# DF2$Size <- 'Adjusted'
+# DF2$Name <- NA
+#
+# for (i in 1:nrow(DF2)) {
+#   vals <- as.numeric(DF1[i,2:7])
+#   subDF <- DF2 %>% filter(M==vals[1],
+#                           sigmaR==vals[2],
+#                           h==vals[3],
+#                           lambda==vals[4],
+#                           incQ==vals[5],
+#                           incE==vals[6])
+#   nm <- DF1$Name[i]
+#   DF2$Name[subDF$i] <- paste0(nm, '_adj')
+# }
+#
+# DF_all <- dplyr::bind_rows(DF1, DF2)
+# DF_all <- DF_all %>% arrange(Name)
+# saveRDS(DF_all, 'inst/shiny_apps/SWOMSE/data/OMTable.rds')
+
+
+
+# OM_Axes <- readRDS('inst/shiny_apps/SWOMSE/data/OMTable.rds')
+OM_Axes <- readRDS('data/OMTable.rds')
+OM_Axes$incE <- as.logical(OM_Axes$incE)
+
+OM_Axes$incQ[OM_Axes$incQ == 1] <- FALSE
+OM_Axes$incQ[OM_Axes$incQ>1] <- TRUE
+OM_Axes$incQ <- as.logical(OM_Axes$incQ)
+
+OMnames <- OM_Axes$Name
+
+PMnames <- avail("PM")
+
+joinMSE2 <- function (MSEobjs = NULL)
+{
+  if (class(MSEobjs) != "list")
+    stop("MSEobjs must be a list")
+  if (length(MSEobjs) < 2)
+    stop("MSEobjs list doesn't contain multiple MSE objects")
+  lapply(MSEobjs, checkMSE)
+  if (!all(unlist(lapply(MSEobjs, class)) == "MSE"))
+    stop("Objects in list must all be class `MSE`")
+  MPNames <- lapply(MSEobjs, getElement, name = "MPs")
+  allsame <- length(unique(lapply(MPNames, unique))) == 1
+  if (!allsame) {
+    mpnames <- unlist(MPNames)
+    npack <- length(MSEobjs)
+    tab <- table(mpnames)
+    ind <- tab == npack
+    commonMPs <- names(tab)[ind]
+    if (length(commonMPs) < 1)
+      stop("No common MPs in MSE objects", call. = FALSE)
+    MSEobjs <- lapply(MSEobjs, Sub, MPs = commonMPs)
+    message("MPs not in all MSE objects:")
+    message(paste(names(tab)[!ind], ""))
+    message("Dropped from final MSE object.")
+  }
+  Nobjs <- length(MSEobjs)
+  for (X in 1:Nobjs) {
+    tt <- MSEobjs[[X]]
+    assign(paste0("obj", X), tt)
+    if (X > 1) {
+      tt <- MSEobjs[[X]]
+      tt2 <- MSEobjs[[X - 1]]
+      if (!all(slotNames(tt) == slotNames(tt2)))
+        stop("The MSE objects don't have the same slots")
+      if (any(tt@MPs != tt2@MPs))
+        stop("MPs must be the same for all MSE objects")
     }
   }
-  DFuncert <- do.call('rbind', outlist)
+  chkmat <- matrix(NA, nrow = Nobjs, ncol = 2)
+  nms <- NULL
+  for (X in 1:Nobjs) {
+    tt <- get(paste0("obj", X))
+    chkmat[X, ] <- c(tt@nyears, tt@proyears)
+    if (X > 1)
+      if (!any(grepl(tt@Name, nms)))
+        stop("MSE objects have different names")
+    nms <- append(nms, tt@Name)
+  }
+  chk <- all(colSums(chkmat) == chkmat[1, ] * Nobjs)
+  if (!chk)
+    stop("The MSE objects have different number of nyears or proyears")
+  Allobjs <- mget(paste0("obj", 1:Nobjs))
+  Name <- Allobjs[[1]]@Name
+  nyears <- Allobjs[[1]]@nyears
+  proyears <- Allobjs[[1]]@proyears
+  nMPs <- length(MPNames[[1]])
+  MPs <- MPNames[[1]]
+  nsim <- sum(sapply(Allobjs, slot, name = "nsim"))
+  OM <- do.call("rbind", lapply(Allobjs, slot, name = "OM"))
+  Obs <- do.call("rbind", lapply(Allobjs, slot, name = "Obs"))
+  SB_SBMSY <- abind::abind(lapply(Allobjs, slot, name = "SB_SBMSY"),
+                           along = 1)
+  F_FMSY <- abind::abind(lapply(Allobjs, slot, name = "F_FMSY"),
+                         along = 1)
+  N <- abind::abind(lapply(Allobjs, slot, name = "N"), along = 1)
+  B <- abind::abind(lapply(Allobjs, slot, name = "B"), along = 1)
+  SSB <- abind::abind(lapply(Allobjs, slot, name = "SSB"),
+                      along = 1)
+  VB <- abind::abind(lapply(Allobjs, slot, name = "VB"), along = 1)
+  FM <- abind::abind(lapply(Allobjs, slot, name = "FM"), along = 1)
+  SPR <- list()
+  Catch <- abind::abind(lapply(Allobjs, slot, name = "Catch"),
+                        along = 1)
+  Removals <- abind::abind(lapply(Allobjs, slot, name = "Removals"),
+                           along = 1)
+  Effort <- abind::abind(lapply(Allobjs, slot, name = "Effort"),
+                         along = 1)
+  TAC <- abind::abind(lapply(Allobjs, slot, name = "TAC"),
+                      along = 1)
+  TAE <- abind::abind(lapply(Allobjs, slot, name = "TAE"),
+                      along = 1)
+  BioEco <- list()
+  BioEco_List <- lapply(Allobjs, slot, name = "BioEco")
+  nms <- names(Allobjs[[1]]@BioEco)
+  for (nm in nms) {
+    temp <- list()
+    for (obj in 1:length(Allobjs)) {
+      temp[[obj]] <- BioEco_List[[obj]][[nm]]
+    }
+    BioEco[[nm]] <- abind::abind(temp, along = 1)
+  }
+  RefPoint <- list()
+  RefPoint_List <- lapply(Allobjs, slot, name = "RefPoint")
+  nms <- names(Allobjs[[1]]@RefPoint)
+  for (nm in nms) {
+    temp <- list()
+    for (obj in 1:length(Allobjs)) {
+      temp[[obj]] <- RefPoint_List[[obj]][[nm]]
+    }
+    if (class(temp[[1]]) == "list") {
+      nms2 <- names(temp[[1]])
+      for (j in seq_along(nms2)) {
+        temp2 <- list()
+        for (k in 1:length(temp)) {
+          temp2[[k]] <- temp[[k]][[j]]
+        }
+        nm2 <- nms2[j]
+        RefPoint[[nm]][[nm2]] <- abind::abind(temp2,
+                                              along = 1)
+      }
+    }
+    else {
+      RefPoint[[nm]] <- abind::abind(temp, along = 1)
+    }
+  }
+  CB_hist <- abind::abind(lapply(Allobjs, slot, name = "CB_hist"),
+                          along = 1)
+  FM_hist <- abind::abind(lapply(Allobjs, slot, name = "FM_hist"),
+                          along = 1)
+  SSB_hist <- abind::abind(lapply(Allobjs, slot, name = "SSB_hist"),
+                           along = 1)
 
-  DF <- bind_rows(DFbc, DFuncert)
-  DF <- arrange(DF, Num)
+  PPD <- list()
+  Hist <- new('Hist')
 
-  saveRDS(DF, "inst/shiny_apps/SWOMSE/data/OMTable.rds")
+  Misc <- list()
+  MSE <- new("MSE", Name, nyears, proyears, nMPs, MPs, nsim,
+             OM, Obs, SB_SBMSY, F_FMSY, N, B, SSB, VB, FM, SPR, Catch,
+             Removals, Effort, TAC, TAE, BioEco, RefPoint, CB_hist,
+             FM_hist, SSB_hist, Hist, PPD, Misc)
+  MSE
 }
-# Run function to save parameter assumption table
- # makeDF(grid.dir='G:/My Drive/1_PROJECTS/North_Atlantic_Swordfish/OMs/Grid_2020/grid_2020')
-
-# OMAssumption <- readRDS('inst/shiny_apps/SWOMSE/data/OMTable.rds')
-OMAssumption <- readRDS('data/OMTable.rds')
-OMAssumption$env[OMAssumption$env<0] <- FALSE
-OMAssumption$env[OMAssumption$env>0] <- TRUE
-OMAssumption$env <- as.logical(OMAssumption$env)
-
-OMAssumption$llq[OMAssumption$llq == 1] <- FALSE
-OMAssumption$llq[OMAssumption$llq>1] <- TRUE
-OMAssumption$llq <- as.logical(OMAssumption$llq)
-
-OMnames <- OMAssumption$Name
-
-PMnames <- avail("PM", 'all')
--- inst/shiny_apps/SWOMSE/server.R
+++ inst/shiny_apps/SWOMSE/server.R
@@ -4,64 +4,66 @@ library(shiny)
 shinyServer(function(input, output, session) {
 
   # ---- OM 1 ----
+  # Size Data
+  output$Size_cb_1 <- renderUI({
+    choices <- unique(OM_Axes$Size) %>% sort()
+    checkboxGroupInput("Size_cb_1","Size Composition Data", choices = choices, selected = choices[1])
+  })
+
   # Natural Mortality
   output$M_cb_1 <- renderUI({
-    choices <- unique(OMAssumption$M) %>% sort()
+    choices <- unique(OM_Axes$M) %>% sort()
     checkboxGroupInput("M_cb_1","Natural mortality (M)", choices = choices, selected = choices[1])
   })
   # sigma R
   output$sigmaR_cb_1 <- renderUI({
-    choices <- unique(OMAssumption$sigmaR) %>% sort()
+    choices <- unique(OM_Axes$sigmaR) %>% sort()
     checkboxGroupInput("sigmaR_cb_1","Recruitment variability (sigmaR)", choices = choices, selected = choices[1])
   })
   # steepness
   output$steepness_cb_1 <- renderUI({
-    choices <- unique(OMAssumption$steepness) %>% sort()
+    choices <- unique(OM_Axes$h) %>% sort()
     checkboxGroupInput("steepness_cb_1","Steepness (h)", choices = choices, selected = choices[1])
   })
-  # cpuecv
-  output$cpuecv_cb_1 <- renderUI({
-    choices <- unique(OMAssumption$cpuecv) %>% sort()
-    checkboxGroupInput("cpuecv_cb_1","CPUE CV", choices = choices, selected = choices[1])
+  # Lambda - CPUE:CAL weighting
+  output$lambda_cb_1 <- renderUI({
+    choices <- unique(OM_Axes$lambda) %>% sort()
+    checkboxGroupInput("lambda_cb_1","CPUE:CAL Weighting", choices = choices, selected = choices[1])
   })
-  # Effective sample size
-  output$ESS_cb_1 <- renderUI({
-    choices <- unique(OMAssumption$ess) %>% sort()
-    checkboxGroupInput("ESS_cb_1","Effective Sample Size", choices = choices, selected = choices[1])
-  })
+
   # Increase in q
   output$llq_cb_1 <- renderUI({
-    choices <- unique(OMAssumption$llq) %>% sort()
+    choices <- unique(OM_Axes$incQ) %>% sort()
     checkboxGroupInput("llq_cb_1","Increase in q?", choices = choices, selected = choices[1])
   })
   # Environmental covariate
   output$env_cb_1 <- renderUI({
-    choices <- unique(OMAssumption$env) %>% sort()
+    choices <- unique(OM_Axes$incE) %>% sort()
     checkboxGroupInput("env_cb_1","Environmental covariate?", choices = choices, selected = choices[1])
   })
 
   # Observe events
   observeEvent(input$OM1,{
     OMind<-match(input$OM1,OMnames)
-    updateCheckboxGroupInput(session,"M_cb_1",selected=OMAssumption$M[OMind])
-    updateCheckboxGroupInput(session,"sigmaR_cb_1",selected=OMAssumption$sigmaR[OMind])
-    updateCheckboxGroupInput(session,"steepness_cb_1",selected=OMAssumption$steepness[OMind])
-    updateCheckboxGroupInput(session,"cpuecv_cb_1",selected=OMAssumption$cpuecv[OMind])
-    updateCheckboxGroupInput(session,"ESS_cb_1",selected=OMAssumption$ess[OMind])
-    updateCheckboxGroupInput(session,"llq_cb_1",selected=OMAssumption$llq[OMind])
-    updateCheckboxGroupInput(session,"env_cb_1",selected=OMAssumption$env[OMind])
+    updateCheckboxGroupInput(session,"Size_cb_1",selected=OM_Axes$Size[OMind])
+    updateCheckboxGroupInput(session,"M_cb_1",selected=OM_Axes$M[OMind])
+    updateCheckboxGroupInput(session,"sigmaR_cb_1",selected=OM_Axes$sigmaR[OMind])
+    updateCheckboxGroupInput(session,"steepness_cb_1",selected=OM_Axes$h[OMind])
+    updateCheckboxGroupInput(session,"lambda_cb_1",selected=OM_Axes$lambda[OMind])
+    updateCheckboxGroupInput(session,"llq_cb_1",selected=OM_Axes$incQ[OMind])
+    updateCheckboxGroupInput(session,"env_cb_1",selected=OM_Axes$incE[OMind])
 
   })
 
   # Get OM Numbers
   getOM_1 <-function() {
-    OMAssumption$Name[OMAssumption$M %in% input$M_cb_1 &
-                       OMAssumption$sigmaR %in% input$sigmaR_cb_1 &
-                       OMAssumption$steepness %in% input$steepness_cb_1 &
-                       OMAssumption$cpuecv %in% input$cpuecv_cb_1 &
-                       OMAssumption$ess %in% input$ESS_cb_1 &
-                       OMAssumption$llq %in% input$llq_cb_1 &
-                       OMAssumption$env %in% input$env_cb_1]
+    OM_Axes$Name[OM_Axes$Size %in% input$Size_cb_1 &
+                   OM_Axes$M %in% input$M_cb_1 &
+                   OM_Axes$sigmaR %in% input$sigmaR_cb_1 &
+                   OM_Axes$h %in% input$steepness_cb_1 &
+                   OM_Axes$lambda %in% input$lambda_cb_1 &
+                   OM_Axes$incQ %in% input$llq_cb_1 &
+                   OM_Axes$incE %in% input$env_cb_1]
   }
 
   output$OM1_nums<-renderText({
@@ -70,85 +72,103 @@ shinyServer(function(input, output, session) {
 
 
   # ---- OM 2 ----
+  # Size Data
+  output$Size_cb_2 <- renderUI({
+    choices <- unique(OM_Axes$Size) %>% sort()
+    checkboxGroupInput("Size_cb_2","Size Composition Data", choices = choices, selected = choices[1])
+  })
+
   # Natural Mortality
   output$M_cb_2 <- renderUI({
-    choices <- unique(OMAssumption$M) %>% sort()
+    choices <- unique(OM_Axes$M) %>% sort()
     checkboxGroupInput("M_cb_2","Natural mortality (M)", choices = choices, selected = choices[1])
   })
   # sigma R
   output$sigmaR_cb_2 <- renderUI({
-    choices <- unique(OMAssumption$sigmaR) %>% sort()
+    choices <- unique(OM_Axes$sigmaR) %>% sort()
     checkboxGroupInput("sigmaR_cb_2","Recruitment variability (sigmaR)", choices = choices, selected = choices[1])
   })
   # steepness
   output$steepness_cb_2 <- renderUI({
-    choices <- unique(OMAssumption$steepness) %>% sort()
+    choices <- unique(OM_Axes$h) %>% sort()
     checkboxGroupInput("steepness_cb_2","Steepness (h)", choices = choices, selected = choices[1])
   })
-  # cpuecv
-  output$cpuecv_cb_2 <- renderUI({
-    choices <- unique(OMAssumption$cpuecv) %>% sort()
-    checkboxGroupInput("cpuecv_cb_2","CPUE CV", choices = choices, selected = choices[1])
+  # Lambda - CPUE:CAL weighting
+  output$lambda_cb_2 <- renderUI({
+    choices <- unique(OM_Axes$lambda) %>% sort()
+    checkboxGroupInput("lambda_cb_2","CPUE:CAL Weighting", choices = choices, selected = choices[1])
   })
-  # Effective sample size
-  output$ESS_cb_2 <- renderUI({
-    choices <- unique(OMAssumption$ess) %>% sort()
-    checkboxGroupInput("ESS_cb_2","Effective Sample Size", choices = choices, selected = choices[1])
-  })
+
   # Increase in q
   output$llq_cb_2 <- renderUI({
-    choices <- unique(OMAssumption$llq) %>% sort()
+    choices <- unique(OM_Axes$incQ) %>% sort()
     checkboxGroupInput("llq_cb_2","Increase in q?", choices = choices, selected = choices[1])
   })
   # Environmental covariate
   output$env_cb_2 <- renderUI({
-    choices <- unique(OMAssumption$env) %>% sort()
+    choices <- unique(OM_Axes$incE) %>% sort()
     checkboxGroupInput("env_cb_2","Environmental covariate?", choices = choices, selected = choices[1])
   })
 
   # Observe events
   observeEvent(input$OM2,{
     OMind<-match(input$OM2,OMnames)
-    updateCheckboxGroupInput(session,"M_cb_2",selected=OMAssumption$M[OMind])
-    updateCheckboxGroupInput(session,"sigmaR_cb_2",selected=OMAssumption$sigmaR[OMind])
-    updateCheckboxGroupInput(session,"steepness_cb_2",selected=OMAssumption$steepness[OMind])
-    updateCheckboxGroupInput(session,"cpuecv_cb_2",selected=OMAssumption$cpuecv[OMind])
-    updateCheckboxGroupInput(session,"ESS_cb_2",selected=OMAssumption$ess[OMind])
-    updateCheckboxGroupInput(session,"llq_cb_2",selected=OMAssumption$llq[OMind])
-    updateCheckboxGroupInput(session,"env_cb_2",selected=OMAssumption$env[OMind])
+    updateCheckboxGroupInput(session,"Size_cb_2",selected=OM_Axes$Size[OMind])
+    updateCheckboxGroupInput(session,"M_cb_2",selected=OM_Axes$M[OMind])
+    updateCheckboxGroupInput(session,"sigmaR_cb_2",selected=OM_Axes$sigmaR[OMind])
+    updateCheckboxGroupInput(session,"steepness_cb_2",selected=OM_Axes$h[OMind])
+    updateCheckboxGroupInput(session,"lambda_cb_2",selected=OM_Axes$lambda[OMind])
+    updateCheckboxGroupInput(session,"llq_cb_2",selected=OM_Axes$incQ[OMind])
+    updateCheckboxGroupInput(session,"env_cb_2",selected=OM_Axes$incE[OMind])
 
   })
+
   # Get OM Numbers
   getOM_2 <-function() {
-    OMAssumption$Name[OMAssumption$M %in% input$M_cb_2 &
-                       OMAssumption$sigmaR %in% input$sigmaR_cb_2 &
-                       OMAssumption$steepness %in% input$steepness_cb_2 &
-                       OMAssumption$cpuecv %in% input$cpuecv_cb_2 &
-                       OMAssumption$ess %in% input$ESS_cb_2 &
-                       OMAssumption$llq %in% input$llq_cb_2 &
-                       OMAssumption$env %in% input$env_cb_2]
+    OM_Axes$Name[OM_Axes$Size %in% input$Size_cb_2 &
+                   OM_Axes$M %in% input$M_cb_2 &
+                   OM_Axes$sigmaR %in% input$sigmaR_cb_2 &
+                   OM_Axes$h %in% input$steepness_cb_2 &
+                   OM_Axes$lambda %in% input$lambda_cb_2 &
+                   OM_Axes$incQ %in% input$llq_cb_2 &
+                   OM_Axes$incE %in% input$env_cb_2]
   }
+
 
   output$OM2_nums<-renderText({
     getOM_2()
   })
 
-  # SubMSE
-  SubMSE <- function(SWO_MSE, names) {
-    sims <- which(SWOM@Source %in% names)
 
-    MSE <- MSEtool::Sub(SWO_MSE, sims=sims)
-    if (MSE@nsim ==1) {
-      # MSE@Misc$LatEffort <- array(MSE@Misc$LatEffort, dim=c(1, dim( MSE@Misc$LatEffort)))
-      MSE <- joinMSE(list(MSE, MSE)) # temp fix for only 1 sim per OM
+  # Load MSEs
+  LoadMSE <- function(names) {
+    List <- list()
+    for (i in seq_along(names)) {
+      row <- match(names[i], OM_Axes$Name)
+      df <- OM_Axes[row,]
+      if (df$Size=='Adjusted') {
+        mse <- readRDS(paste0(file.path('data/grid_May2021_shifted/', df$i, '/MSE.rda')))
+        mse@Name <-'name'
+        List[[i]] <- mse
+      } else {
+        mse <- readRDS(paste0(file.path('data/grid_May2021/', df$i, '/MSE.rda')))
+        mse@Name <- 'name'
+        List[[i]] <- mse
+      }
+    }
+    if (length(List)>1) {
+      MSE <- joinMSE2(List)
+    } else {
+      MSE <- List[[1]]
     }
     MSE
   }
 
+
   # Performance table
   makeTable <- function(names) {
 
-    MSE <- SubMSE(SWO_MSE, names)
+    MSE <- LoadMSE(names)
     tt <- summary(MSE)
     tt
   }
@@ -156,10 +176,12 @@ shinyServer(function(input, output, session) {
                                         options = list(dom = 't'))
   output$PTable2 <- DT::renderDataTable(makeTable(getOM_2()),
                                         options = list(dom = 't'))
+
+
   # Zeh plots
   # Wormplots
   makeZehPlot <- function(names) {
-    MSE <- SubMSE(SWO_MSE, names)
+    MSE <- LoadMSE(names)
     MSEtool::DFO_bar(MSE)
   }
 
@@ -168,7 +190,7 @@ shinyServer(function(input, output, session) {
 
   # Wormplots
   makeWormPlot <- function(names) {
-    MSE <- SubMSE(SWO_MSE, names)
+    MSE <- LoadMSE(names)
     MSEtool::wormplot(MSE)
   }
   output$wormplot1 <- renderPlot(makeWormPlot(getOM_1()))
@@ -176,7 +198,7 @@ shinyServer(function(input, output, session) {
 
   # Trade-off plots
   makeTradeoffPlot <- function(names, PM1, PM2) {
-    MSE <<- SubMSE(SWO_MSE, names)
+    MSE <- LoadMSE(names)
     TradePlot(MSE, Lims=0, PMlist=c(PM1, PM2), legend = FALSE)
   }
 
