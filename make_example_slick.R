8e63a3c38e0f822ddf672fbe07fae64a843e037f
Adrian <ar.hordyk@gmail.com>
2022-03-24 14:51:15 -0700

    Create make_example_slick.R

---
+++ make_example_slick.R
@@ -0,0 +1,10 @@
+
+library(SWOMSE)
+
+# install Slick package
+# devtools::install_github('blue-matter/Slick')
+
+library(Slick)
+
+# Copy and modify example in G:\Shared drives\BM shared\1. Projects\SLICK\Case Studies\SWO
+
