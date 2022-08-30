89a7e0ecfd0b74a25c1ff710c502f40e41ab2adb
Adrian <ar.hordyk@gmail.com>
2022-08-30 09:15:17 -0700

    Update Roxygen.r

--- R/Roxygen.r
+++ R/Roxygen.r
@@ -2,465 +2,6 @@
 # Don't edit by hand!
 # 
 
-#' @name SWOData
-#' @docType data
-#' @title North Atlantic Swordfish Data
-#' @description North Atlantic Swordfish Data
-#'  
-NULL
-
-
-#' @name SWO-OMs
-#' @docType data
-#' @title Operating models
-#' @description North Atlantic Swordfish Operating Models
-#'  
-#' `SWOM` is a single OM combining all SS OMs including `OM_base_case` and
-#' all SS OMs there were imported into the package (`OM_1`, `OM_2`, ..., etc)
-NULL
-
-
-#' @rdname SWO-OMs 
-'OM_1'
-#' @rdname SWO-OMs 
-'OM_2'
-#' @rdname SWO-OMs 
-'OM_3'
-#' @rdname SWO-OMs 
-'OM_4'
-#' @rdname SWO-OMs 
-'OM_5'
-#' @rdname SWO-OMs 
-'OM_6'
-#' @rdname SWO-OMs 
-'OM_7'
-#' @rdname SWO-OMs 
-'OM_8'
-#' @rdname SWO-OMs 
-'OM_9'
-#' @rdname SWO-OMs 
-'OM_10'
-#' @rdname SWO-OMs 
-'OM_11'
-#' @rdname SWO-OMs 
-'OM_12'
-#' @rdname SWO-OMs 
-'OM_13'
-#' @rdname SWO-OMs 
-'OM_14'
-#' @rdname SWO-OMs 
-'OM_15'
-#' @rdname SWO-OMs 
-'OM_16'
-#' @rdname SWO-OMs 
-'OM_17'
-#' @rdname SWO-OMs 
-'OM_18'
-#' @rdname SWO-OMs 
-'OM_19'
-#' @rdname SWO-OMs 
-'OM_20'
-#' @rdname SWO-OMs 
-'OM_21'
-#' @rdname SWO-OMs 
-'OM_22'
-#' @rdname SWO-OMs 
-'OM_23'
-#' @rdname SWO-OMs 
-'OM_24'
-#' @rdname SWO-OMs 
-'OM_25'
-#' @rdname SWO-OMs 
-'OM_26'
-#' @rdname SWO-OMs 
-'OM_27'
-#' @rdname SWO-OMs 
-'OM_28'
-#' @rdname SWO-OMs 
-'OM_29'
-#' @rdname SWO-OMs 
-'OM_30'
-#' @rdname SWO-OMs 
-'OM_31'
-#' @rdname SWO-OMs 
-'OM_32'
-#' @rdname SWO-OMs 
-'OM_33'
-#' @rdname SWO-OMs 
-'OM_34'
-#' @rdname SWO-OMs 
-'OM_35'
-#' @rdname SWO-OMs 
-'OM_36'
-#' @rdname SWO-OMs 
-'OM_37'
-#' @rdname SWO-OMs 
-'OM_38'
-#' @rdname SWO-OMs 
-'OM_39'
-#' @rdname SWO-OMs 
-'OM_40'
-#' @rdname SWO-OMs 
-'OM_41'
-#' @rdname SWO-OMs 
-'OM_42'
-#' @rdname SWO-OMs 
-'OM_43'
-#' @rdname SWO-OMs 
-'OM_44'
-#' @rdname SWO-OMs 
-'OM_45'
-#' @rdname SWO-OMs 
-'OM_46'
-#' @rdname SWO-OMs 
-'OM_47'
-#' @rdname SWO-OMs 
-'OM_48'
-#' @rdname SWO-OMs 
-'OM_49'
-#' @rdname SWO-OMs 
-'OM_50'
-#' @rdname SWO-OMs 
-'OM_51'
-#' @rdname SWO-OMs 
-'OM_52'
-#' @rdname SWO-OMs 
-'OM_53'
-#' @rdname SWO-OMs 
-'OM_54'
-#' @rdname SWO-OMs 
-'OM_55'
-#' @rdname SWO-OMs 
-'OM_56'
-#' @rdname SWO-OMs 
-'OM_57'
-#' @rdname SWO-OMs 
-'OM_58'
-#' @rdname SWO-OMs 
-'OM_59'
-#' @rdname SWO-OMs 
-'OM_60'
-#' @rdname SWO-OMs 
-'OM_61'
-#' @rdname SWO-OMs 
-'OM_62'
-#' @rdname SWO-OMs 
-'OM_63'
-#' @rdname SWO-OMs 
-'OM_64'
-#' @rdname SWO-OMs 
-'OM_65'
-#' @rdname SWO-OMs 
-'OM_66'
-#' @rdname SWO-OMs 
-'OM_67'
-#' @rdname SWO-OMs 
-'OM_68'
-#' @rdname SWO-OMs 
-'OM_69'
-#' @rdname SWO-OMs 
-'OM_70'
-#' @rdname SWO-OMs 
-'OM_71'
-#' @rdname SWO-OMs 
-'OM_72'
-#' @rdname SWO-OMs 
-'OM_73'
-#' @rdname SWO-OMs 
-'OM_74'
-#' @rdname SWO-OMs 
-'OM_75'
-#' @rdname SWO-OMs 
-'OM_76'
-#' @rdname SWO-OMs 
-'OM_77'
-#' @rdname SWO-OMs 
-'OM_78'
-#' @rdname SWO-OMs 
-'OM_79'
-#' @rdname SWO-OMs 
-'OM_80'
-#' @rdname SWO-OMs 
-'OM_81'
-#' @rdname SWO-OMs 
-'OM_82'
-#' @rdname SWO-OMs 
-'OM_83'
-#' @rdname SWO-OMs 
-'OM_84'
-#' @rdname SWO-OMs 
-'OM_85'
-#' @rdname SWO-OMs 
-'OM_86'
-#' @rdname SWO-OMs 
-'OM_87'
-#' @rdname SWO-OMs 
-'OM_88'
-#' @rdname SWO-OMs 
-'OM_89'
-#' @rdname SWO-OMs 
-'OM_90'
-#' @rdname SWO-OMs 
-'OM_91'
-#' @rdname SWO-OMs 
-'OM_92'
-#' @rdname SWO-OMs 
-'OM_93'
-#' @rdname SWO-OMs 
-'OM_94'
-#' @rdname SWO-OMs 
-'OM_95'
-#' @rdname SWO-OMs 
-'OM_96'
-#' @rdname SWO-OMs 
-'OM_97'
-#' @rdname SWO-OMs 
-'OM_98'
-#' @rdname SWO-OMs 
-'OM_99'
-#' @rdname SWO-OMs 
-'OM_100'
-#' @rdname SWO-OMs 
-'OM_101'
-#' @rdname SWO-OMs 
-'OM_102'
-#' @rdname SWO-OMs 
-'OM_103'
-#' @rdname SWO-OMs 
-'OM_104'
-#' @rdname SWO-OMs 
-'OM_105'
-#' @rdname SWO-OMs 
-'OM_106'
-#' @rdname SWO-OMs 
-'OM_107'
-#' @rdname SWO-OMs 
-'OM_108'
-#' @rdname SWO-OMs 
-'OM_109'
-#' @rdname SWO-OMs 
-'OM_110'
-#' @rdname SWO-OMs 
-'OM_111'
-#' @rdname SWO-OMs 
-'OM_112'
-#' @rdname SWO-OMs 
-'OM_113'
-#' @rdname SWO-OMs 
-'OM_114'
-#' @rdname SWO-OMs 
-'OM_115'
-#' @rdname SWO-OMs 
-'OM_116'
-#' @rdname SWO-OMs 
-'OM_117'
-#' @rdname SWO-OMs 
-'OM_118'
-#' @rdname SWO-OMs 
-'OM_119'
-#' @rdname SWO-OMs 
-'OM_120'
-#' @rdname SWO-OMs 
-'OM_121'
-#' @rdname SWO-OMs 
-'OM_122'
-#' @rdname SWO-OMs 
-'OM_123'
-#' @rdname SWO-OMs 
-'OM_124'
-#' @rdname SWO-OMs 
-'OM_125'
-#' @rdname SWO-OMs 
-'OM_126'
-#' @rdname SWO-OMs 
-'OM_127'
-#' @rdname SWO-OMs 
-'OM_128'
-#' @rdname SWO-OMs 
-'OM_129'
-#' @rdname SWO-OMs 
-'OM_130'
-#' @rdname SWO-OMs 
-'OM_131'
-#' @rdname SWO-OMs 
-'OM_132'
-#' @rdname SWO-OMs 
-'OM_133'
-#' @rdname SWO-OMs 
-'OM_134'
-#' @rdname SWO-OMs 
-'OM_135'
-#' @rdname SWO-OMs 
-'OM_136'
-#' @rdname SWO-OMs 
-'OM_137'
-#' @rdname SWO-OMs 
-'OM_138'
-#' @rdname SWO-OMs 
-'OM_139'
-#' @rdname SWO-OMs 
-'OM_140'
-#' @rdname SWO-OMs 
-'OM_141'
-#' @rdname SWO-OMs 
-'OM_142'
-#' @rdname SWO-OMs 
-'OM_143'
-#' @rdname SWO-OMs 
-'OM_144'
-#' @rdname SWO-OMs 
-'OM_145'
-#' @rdname SWO-OMs 
-'OM_146'
-#' @rdname SWO-OMs 
-'OM_147'
-#' @rdname SWO-OMs 
-'OM_148'
-#' @rdname SWO-OMs 
-'OM_149'
-#' @rdname SWO-OMs 
-'OM_150'
-#' @rdname SWO-OMs 
-'OM_151'
-#' @rdname SWO-OMs 
-'OM_152'
-#' @rdname SWO-OMs 
-'OM_153'
-#' @rdname SWO-OMs 
-'OM_154'
-#' @rdname SWO-OMs 
-'OM_155'
-#' @rdname SWO-OMs 
-'OM_156'
-#' @rdname SWO-OMs 
-'OM_157'
-#' @rdname SWO-OMs 
-'OM_158'
-#' @rdname SWO-OMs 
-'OM_159'
-#' @rdname SWO-OMs 
-'OM_160'
-#' @rdname SWO-OMs 
-'OM_161'
-#' @rdname SWO-OMs 
-'OM_162'
-#' @rdname SWO-OMs 
-'OM_163'
-#' @rdname SWO-OMs 
-'OM_164'
-#' @rdname SWO-OMs 
-'OM_165'
-#' @rdname SWO-OMs 
-'OM_166'
-#' @rdname SWO-OMs 
-'OM_167'
-#' @rdname SWO-OMs 
-'OM_168'
-#' @rdname SWO-OMs 
-'OM_169'
-#' @rdname SWO-OMs 
-'OM_170'
-#' @rdname SWO-OMs 
-'OM_171'
-#' @rdname SWO-OMs 
-'OM_172'
-#' @rdname SWO-OMs 
-'OM_173'
-#' @rdname SWO-OMs 
-'OM_174'
-#' @rdname SWO-OMs 
-'OM_175'
-#' @rdname SWO-OMs 
-'OM_176'
-#' @rdname SWO-OMs 
-'OM_177'
-#' @rdname SWO-OMs 
-'OM_178'
-#' @rdname SWO-OMs 
-'OM_179'
-#' @rdname SWO-OMs 
-'OM_180'
-#' @rdname SWO-OMs 
-'OM_181'
-#' @rdname SWO-OMs 
-'OM_182'
-#' @rdname SWO-OMs 
-'OM_183'
-#' @rdname SWO-OMs 
-'OM_184'
-#' @rdname SWO-OMs 
-'OM_185'
-#' @rdname SWO-OMs 
-'OM_186'
-#' @rdname SWO-OMs 
-'OM_187'
-#' @rdname SWO-OMs 
-'OM_188'
-#' @rdname SWO-OMs 
-'OM_189'
-#' @rdname SWO-OMs 
-'OM_190'
-#' @rdname SWO-OMs 
-'OM_191'
-#' @rdname SWO-OMs 
-'OM_192'
-#' @rdname SWO-OMs 
-'OM_193'
-#' @rdname SWO-OMs 
-'OM_194'
-#' @rdname SWO-OMs 
-'OM_195'
-#' @rdname SWO-OMs 
-'OM_196'
-#' @rdname SWO-OMs 
-'OM_197'
-#' @rdname SWO-OMs 
-'OM_198'
-#' @rdname SWO-OMs 
-'OM_199'
-#' @rdname SWO-OMs 
-'OM_200'
-#' @rdname SWO-OMs 
-'OM_201'
-#' @rdname SWO-OMs 
-'OM_202'
-#' @rdname SWO-OMs 
-'OM_203'
-#' @rdname SWO-OMs 
-'OM_204'
-#' @rdname SWO-OMs 
-'OM_205'
-#' @rdname SWO-OMs 
-'OM_206'
-#' @rdname SWO-OMs 
-'OM_207'
-#' @rdname SWO-OMs 
-'OM_208'
-#' @rdname SWO-OMs 
-'OM_209'
-#' @rdname SWO-OMs 
-'OM_210'
-#' @rdname SWO-OMs 
-'OM_211'
-#' @rdname SWO-OMs 
-'OM_212'
-#' @rdname SWO-OMs 
-'OM_213'
-#' @rdname SWO-OMs 
-'OM_214'
-#' @rdname SWO-OMs 
-'OM_215'
-#' @rdname SWO-OMs 
-'OM_216'
-
-#' @name OM_DF
-#' @docType data
-#' @title North Atlantic Swordfish OM Data-Frame
-#' @description Summary of the North Atlantic Swordfish OM Parameters
-#'  
-NULL
-
-
 
 #' @name Fleet_DF
 #' @docType data
@@ -470,3 +11,466 @@ NULL
  "Fleet_DF"
 
 
+#' @name SWOData
+#' @docType data
+#' @title North Atlantic Swordfish Data
+#' @description North Atlantic Swordfish Data
+#'  
+NULL
+
+
+
+#' @name OM_DF
+#' @docType data
+#' @title North Atlantic Swordfish OM Data-Frame
+#' @description Summary of the North Atlantic Swordfish OM Parameters
+#'  
+NULL
+
+
+#' @name SWO-OMs
+#' @docType data
+#' @title Operating models
+#' @description North Atlantic Swordfish Operating Models
+#'  
+#' `MOM_1` is the base case assessment. The rest of the OMs are from the 
+#' OM uncertainty grid.
+#' 
+#' All OMs are class `MOM` and contain 2 stocks (female and male) and 1 aggregated fleet
+NULL
+
+
+#' @rdname SWO-OMs 
+'MOM_0'
+#' @rdname SWO-OMs 
+'MOM_1'
+#' @rdname SWO-OMs 
+'MOM_2'
+#' @rdname SWO-OMs 
+'MOM_3'
+#' @rdname SWO-OMs 
+'MOM_4'
+#' @rdname SWO-OMs 
+'MOM_5'
+#' @rdname SWO-OMs 
+'MOM_6'
+#' @rdname SWO-OMs 
+'MOM_7'
+#' @rdname SWO-OMs 
+'MOM_8'
+#' @rdname SWO-OMs 
+'MOM_9'
+#' @rdname SWO-OMs 
+'MOM_10'
+#' @rdname SWO-OMs 
+'MOM_11'
+#' @rdname SWO-OMs 
+'MOM_12'
+#' @rdname SWO-OMs 
+'MOM_13'
+#' @rdname SWO-OMs 
+'MOM_14'
+#' @rdname SWO-OMs 
+'MOM_15'
+#' @rdname SWO-OMs 
+'MOM_16'
+#' @rdname SWO-OMs 
+'MOM_17'
+#' @rdname SWO-OMs 
+'MOM_18'
+#' @rdname SWO-OMs 
+'MOM_19'
+#' @rdname SWO-OMs 
+'MOM_20'
+#' @rdname SWO-OMs 
+'MOM_21'
+#' @rdname SWO-OMs 
+'MOM_22'
+#' @rdname SWO-OMs 
+'MOM_23'
+#' @rdname SWO-OMs 
+'MOM_24'
+#' @rdname SWO-OMs 
+'MOM_25'
+#' @rdname SWO-OMs 
+'MOM_26'
+#' @rdname SWO-OMs 
+'MOM_27'
+#' @rdname SWO-OMs 
+'MOM_28'
+#' @rdname SWO-OMs 
+'MOM_29'
+#' @rdname SWO-OMs 
+'MOM_30'
+#' @rdname SWO-OMs 
+'MOM_31'
+#' @rdname SWO-OMs 
+'MOM_32'
+#' @rdname SWO-OMs 
+'MOM_33'
+#' @rdname SWO-OMs 
+'MOM_34'
+#' @rdname SWO-OMs 
+'MOM_35'
+#' @rdname SWO-OMs 
+'MOM_36'
+#' @rdname SWO-OMs 
+'MOM_37'
+#' @rdname SWO-OMs 
+'MOM_38'
+#' @rdname SWO-OMs 
+'MOM_39'
+#' @rdname SWO-OMs 
+'MOM_40'
+#' @rdname SWO-OMs 
+'MOM_41'
+#' @rdname SWO-OMs 
+'MOM_42'
+#' @rdname SWO-OMs 
+'MOM_43'
+#' @rdname SWO-OMs 
+'MOM_44'
+#' @rdname SWO-OMs 
+'MOM_45'
+#' @rdname SWO-OMs 
+'MOM_46'
+#' @rdname SWO-OMs 
+'MOM_47'
+#' @rdname SWO-OMs 
+'MOM_48'
+#' @rdname SWO-OMs 
+'MOM_49'
+#' @rdname SWO-OMs 
+'MOM_50'
+#' @rdname SWO-OMs 
+'MOM_51'
+#' @rdname SWO-OMs 
+'MOM_52'
+#' @rdname SWO-OMs 
+'MOM_53'
+#' @rdname SWO-OMs 
+'MOM_54'
+#' @rdname SWO-OMs 
+'MOM_55'
+#' @rdname SWO-OMs 
+'MOM_56'
+#' @rdname SWO-OMs 
+'MOM_57'
+#' @rdname SWO-OMs 
+'MOM_58'
+#' @rdname SWO-OMs 
+'MOM_59'
+#' @rdname SWO-OMs 
+'MOM_60'
+#' @rdname SWO-OMs 
+'MOM_61'
+#' @rdname SWO-OMs 
+'MOM_62'
+#' @rdname SWO-OMs 
+'MOM_63'
+#' @rdname SWO-OMs 
+'MOM_64'
+#' @rdname SWO-OMs 
+'MOM_65'
+#' @rdname SWO-OMs 
+'MOM_66'
+#' @rdname SWO-OMs 
+'MOM_67'
+#' @rdname SWO-OMs 
+'MOM_68'
+#' @rdname SWO-OMs 
+'MOM_69'
+#' @rdname SWO-OMs 
+'MOM_70'
+#' @rdname SWO-OMs 
+'MOM_71'
+#' @rdname SWO-OMs 
+'MOM_72'
+#' @rdname SWO-OMs 
+'MOM_73'
+#' @rdname SWO-OMs 
+'MOM_74'
+#' @rdname SWO-OMs 
+'MOM_75'
+#' @rdname SWO-OMs 
+'MOM_76'
+#' @rdname SWO-OMs 
+'MOM_77'
+#' @rdname SWO-OMs 
+'MOM_78'
+#' @rdname SWO-OMs 
+'MOM_79'
+#' @rdname SWO-OMs 
+'MOM_80'
+#' @rdname SWO-OMs 
+'MOM_81'
+#' @rdname SWO-OMs 
+'MOM_82'
+#' @rdname SWO-OMs 
+'MOM_83'
+#' @rdname SWO-OMs 
+'MOM_84'
+#' @rdname SWO-OMs 
+'MOM_85'
+#' @rdname SWO-OMs 
+'MOM_86'
+#' @rdname SWO-OMs 
+'MOM_87'
+#' @rdname SWO-OMs 
+'MOM_88'
+#' @rdname SWO-OMs 
+'MOM_89'
+#' @rdname SWO-OMs 
+'MOM_90'
+#' @rdname SWO-OMs 
+'MOM_91'
+#' @rdname SWO-OMs 
+'MOM_92'
+#' @rdname SWO-OMs 
+'MOM_93'
+#' @rdname SWO-OMs 
+'MOM_94'
+#' @rdname SWO-OMs 
+'MOM_95'
+#' @rdname SWO-OMs 
+'MOM_96'
+#' @rdname SWO-OMs 
+'MOM_97'
+#' @rdname SWO-OMs 
+'MOM_98'
+#' @rdname SWO-OMs 
+'MOM_99'
+#' @rdname SWO-OMs 
+'MOM_100'
+#' @rdname SWO-OMs 
+'MOM_101'
+#' @rdname SWO-OMs 
+'MOM_102'
+#' @rdname SWO-OMs 
+'MOM_103'
+#' @rdname SWO-OMs 
+'MOM_104'
+#' @rdname SWO-OMs 
+'MOM_105'
+#' @rdname SWO-OMs 
+'MOM_106'
+#' @rdname SWO-OMs 
+'MOM_107'
+#' @rdname SWO-OMs 
+'MOM_108'
+#' @rdname SWO-OMs 
+'MOM_109'
+#' @rdname SWO-OMs 
+'MOM_110'
+#' @rdname SWO-OMs 
+'MOM_111'
+#' @rdname SWO-OMs 
+'MOM_112'
+#' @rdname SWO-OMs 
+'MOM_113'
+#' @rdname SWO-OMs 
+'MOM_114'
+#' @rdname SWO-OMs 
+'MOM_115'
+#' @rdname SWO-OMs 
+'MOM_116'
+#' @rdname SWO-OMs 
+'MOM_117'
+#' @rdname SWO-OMs 
+'MOM_118'
+#' @rdname SWO-OMs 
+'MOM_119'
+#' @rdname SWO-OMs 
+'MOM_120'
+#' @rdname SWO-OMs 
+'MOM_121'
+#' @rdname SWO-OMs 
+'MOM_122'
+#' @rdname SWO-OMs 
+'MOM_123'
+#' @rdname SWO-OMs 
+'MOM_124'
+#' @rdname SWO-OMs 
+'MOM_125'
+#' @rdname SWO-OMs 
+'MOM_126'
+#' @rdname SWO-OMs 
+'MOM_127'
+#' @rdname SWO-OMs 
+'MOM_128'
+#' @rdname SWO-OMs 
+'MOM_129'
+#' @rdname SWO-OMs 
+'MOM_130'
+#' @rdname SWO-OMs 
+'MOM_131'
+#' @rdname SWO-OMs 
+'MOM_132'
+#' @rdname SWO-OMs 
+'MOM_133'
+#' @rdname SWO-OMs 
+'MOM_134'
+#' @rdname SWO-OMs 
+'MOM_135'
+#' @rdname SWO-OMs 
+'MOM_136'
+#' @rdname SWO-OMs 
+'MOM_137'
+#' @rdname SWO-OMs 
+'MOM_138'
+#' @rdname SWO-OMs 
+'MOM_139'
+#' @rdname SWO-OMs 
+'MOM_140'
+#' @rdname SWO-OMs 
+'MOM_141'
+#' @rdname SWO-OMs 
+'MOM_142'
+#' @rdname SWO-OMs 
+'MOM_143'
+#' @rdname SWO-OMs 
+'MOM_144'
+#' @rdname SWO-OMs 
+'MOM_145'
+#' @rdname SWO-OMs 
+'MOM_146'
+#' @rdname SWO-OMs 
+'MOM_147'
+#' @rdname SWO-OMs 
+'MOM_148'
+#' @rdname SWO-OMs 
+'MOM_149'
+#' @rdname SWO-OMs 
+'MOM_150'
+#' @rdname SWO-OMs 
+'MOM_151'
+#' @rdname SWO-OMs 
+'MOM_152'
+#' @rdname SWO-OMs 
+'MOM_153'
+#' @rdname SWO-OMs 
+'MOM_154'
+#' @rdname SWO-OMs 
+'MOM_155'
+#' @rdname SWO-OMs 
+'MOM_156'
+#' @rdname SWO-OMs 
+'MOM_157'
+#' @rdname SWO-OMs 
+'MOM_158'
+#' @rdname SWO-OMs 
+'MOM_159'
+#' @rdname SWO-OMs 
+'MOM_160'
+#' @rdname SWO-OMs 
+'MOM_161'
+#' @rdname SWO-OMs 
+'MOM_162'
+#' @rdname SWO-OMs 
+'MOM_163'
+#' @rdname SWO-OMs 
+'MOM_164'
+#' @rdname SWO-OMs 
+'MOM_165'
+#' @rdname SWO-OMs 
+'MOM_166'
+#' @rdname SWO-OMs 
+'MOM_167'
+#' @rdname SWO-OMs 
+'MOM_168'
+#' @rdname SWO-OMs 
+'MOM_169'
+#' @rdname SWO-OMs 
+'MOM_170'
+#' @rdname SWO-OMs 
+'MOM_171'
+#' @rdname SWO-OMs 
+'MOM_172'
+#' @rdname SWO-OMs 
+'MOM_173'
+#' @rdname SWO-OMs 
+'MOM_174'
+#' @rdname SWO-OMs 
+'MOM_175'
+#' @rdname SWO-OMs 
+'MOM_176'
+#' @rdname SWO-OMs 
+'MOM_177'
+#' @rdname SWO-OMs 
+'MOM_178'
+#' @rdname SWO-OMs 
+'MOM_179'
+#' @rdname SWO-OMs 
+'MOM_180'
+#' @rdname SWO-OMs 
+'MOM_181'
+#' @rdname SWO-OMs 
+'MOM_182'
+#' @rdname SWO-OMs 
+'MOM_183'
+#' @rdname SWO-OMs 
+'MOM_184'
+#' @rdname SWO-OMs 
+'MOM_185'
+#' @rdname SWO-OMs 
+'MOM_186'
+#' @rdname SWO-OMs 
+'MOM_187'
+#' @rdname SWO-OMs 
+'MOM_188'
+#' @rdname SWO-OMs 
+'MOM_189'
+#' @rdname SWO-OMs 
+'MOM_190'
+#' @rdname SWO-OMs 
+'MOM_191'
+#' @rdname SWO-OMs 
+'MOM_192'
+#' @rdname SWO-OMs 
+'MOM_193'
+#' @rdname SWO-OMs 
+'MOM_194'
+#' @rdname SWO-OMs 
+'MOM_195'
+#' @rdname SWO-OMs 
+'MOM_196'
+#' @rdname SWO-OMs 
+'MOM_197'
+#' @rdname SWO-OMs 
+'MOM_198'
+#' @rdname SWO-OMs 
+'MOM_199'
+#' @rdname SWO-OMs 
+'MOM_200'
+#' @rdname SWO-OMs 
+'MOM_201'
+#' @rdname SWO-OMs 
+'MOM_202'
+#' @rdname SWO-OMs 
+'MOM_203'
+#' @rdname SWO-OMs 
+'MOM_204'
+#' @rdname SWO-OMs 
+'MOM_205'
+#' @rdname SWO-OMs 
+'MOM_206'
+#' @rdname SWO-OMs 
+'MOM_207'
+#' @rdname SWO-OMs 
+'MOM_208'
+#' @rdname SWO-OMs 
+'MOM_209'
+#' @rdname SWO-OMs 
+'MOM_210'
+#' @rdname SWO-OMs 
+'MOM_211'
+#' @rdname SWO-OMs 
+'MOM_212'
+#' @rdname SWO-OMs 
+'MOM_213'
+#' @rdname SWO-OMs 
+'MOM_214'
+#' @rdname SWO-OMs 
+'MOM_215'
+#' @rdname SWO-OMs 
+'MOM_216'
