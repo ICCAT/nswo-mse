c3c3f891bc3712be23a8f749d6dbdd78901b4b73
Adrian <ar.hordyk@gmail.com>
2022-09-21 14:30:37 -0700

    Update Roxygen.r

--- R/Roxygen.r
+++ R/Roxygen.r
@@ -1,13 +1,13 @@
 # This file is automatically built by build_package/build_package.r
 # Don't edit by hand!
-# 
+#
 
 
 #' @name Fleet_DF
 #' @docType data
 #' @title Fleet Information
 #' @description A dataframe of information on fleet names
-#'  
+#'
  "Fleet_DF"
 
 
@@ -15,7 +15,7 @@
 #' @docType data
 #' @title North Atlantic Swordfish Data
 #' @description North Atlantic Swordfish Data
-#'  
+#'
 NULL
 
 
@@ -24,7 +24,7 @@ NULL
 #' @docType data
 #' @title North Atlantic Swordfish OM Data-Frame
 #' @description Summary of the North Atlantic Swordfish OM Parameters
-#'  
+#'
 NULL
 
 
@@ -32,67 +32,85 @@ NULL
 #' @docType data
 #' @title Operating models
 #' @description North Atlantic Swordfish Operating Models
-#'  
-#' `MOM_1` is the base case assessment. The rest of the OMs are from the 
+#'
+#' `MOM_000` is the base case assessment. The rest of the OMs are from the
 #' OM uncertainty grid.
-#' 
+#'
 #' All OMs are class `MOM` and contain 2 stocks (female and male) and 1 aggregated fleet
 NULL
 
 
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_000'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_127'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_128'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_129'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_133'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_134'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_135'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_139'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_140'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_141'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_130'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_131'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_132'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_136'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_137'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_138'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_142'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_143'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_144'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_145'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_146'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_147'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_151'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_152'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_153'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_157'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_158'
-#' @rdname SWO-OMs 
+#' @rdname SWO-OMs
 'MOM_159'
+#' @rdname SWO-OMs
+'MOM_181'
+#' @rdname SWO-OMs
+'MOM_182'
+#' @rdname SWO-OMs
+'MOM_183'
+#' @rdname SWO-OMs
+'MOM_187'
+#' @rdname SWO-OMs
+'MOM_188'
+#' @rdname SWO-OMs
+'MOM_189'
+#' @rdname SWO-OMs
+'MOM_193'
+#' @rdname SWO-OMs
+'MOM_194'
+#' @rdname SWO-OMs
+'MOM_195'
