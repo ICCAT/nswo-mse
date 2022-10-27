66ee0cfa155e50b9d4679b9dd2b7addf95dfd1ca
Adrian <ar.hordyk@gmail.com>
2022-10-27 10:07:00 -0700

    Update Slick_SCRS_P_2022_051.R

--- Slick_objects/SCRS_P_2022_051/Slick_SCRS_P_2022_051.R
+++ Slick_objects/SCRS_P_2022_051/Slick_SCRS_P_2022_051.R
@@ -3,18 +3,25 @@ library(SWOMSE)
 library(Slick)
 
 basedir <- 'Slick_objects/SCRS_P_2022_051'
-MSEdir <- 'results/Reference'
+MSEdir <- 'G:/My Drive/1_PROJECTS/North_Atlantic_Swordfish/Results'
 
-Des<-read.csv(file.path(basedir, 'Design.csv'))
+Des <-read.csv(file.path(basedir, 'Design.csv'))
 
 # --- Make the MSE list ---
+
+# Reference OMs
+Ref_OMs <- list.files(file.path(MSEdir, 'Reference'), pattern='.mmse')
+
+# Robustness
+Rob_OMs <- list.files(file.path(MSEdir, 'Robustness'), pattern='.mmse', recursive = TRUE)
+
 
 MSElist<-list()
 for (i in 1:nrow(Des))   MSElist[[i]]<-readRDS(file.path(MSEdir, paste0(Des$OM[i], '.mmse')))
 
 MPs <- MSElist[[1]]@MPs[[1]]
 MP_Desc <- MPs # TODO add description
-nPerf<-length(MPs)
+
 
 Design = as.matrix(Des[,3:ncol(Des)])
 for(i in 1:ncol(Design))Design[,i]<-match(Design[,i],unique(Design[,i]))
@@ -36,15 +43,21 @@ obj<-Make_Slick(name="SCRS/P/2022/051 Reference OMs",
                 Design = Design,
                 MP_Desc=MP_Desc,
                 SN = list(
-                  Factor_Labels=c("Natural Mortality","Steepness"),
-                  Labels = list(c("M=0.1","M=0.2", 'M=0.3'), c('h=0.6', 'h=0.75', 'h=0.9')),
-                  Codes =  list(c("M1","M2", 'M3'), c('h1', 'h2', 'h3')),
+                  Factor_Labels=c("Natural Mortality","Steepness", "Robustness"),
+                  Labels = list(c("M=0.1","M=0.2", 'M=0.3'),
+                                c('h=0.6', 'h=0.75', 'h=0.9'),
+                                c('Reference', 'Increasing q')),
+                  Codes =  list(c("0.1","0.2", '0.3'),
+                                c('0.6', '0.75', '0.9'),
+                                c('Reference', '1% Increase q')
+                                ),
                   Description = list(paste("Natural mortality rate of",c(0.1, 0.2, 0.3)),
-                                     paste("Beverton-Holt stock recruitment steepness (resilience) of",c(0.6, 0.75, 0.9)))
+                                     paste("Beverton-Holt stock recruitment steepness (resilience) of",c(0.6, 0.75, 0.9)),
+                                     c('Reference Case', 'Assumed 1% Annual Increase in Historical Catchability')
+                                     )
                 ),
                 MSElist=MSElist,
                 fstYr=2021
-
 )
 
 # Standardize Yield
