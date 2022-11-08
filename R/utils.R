adc6314d81133f551562665a1fefef9ac3547c9c
Adrian <ar.hordyk@gmail.com>
2022-11-08 11:29:08 -0800

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,13 @@
 message <- MSEtool:::message
+
+
+#' Access the SWOMSE User Manual
+#'
+#' Opens the `SWOMSE` User Manual in a browser window
+#' @return Nothing.
+#' @export
+#'
+SWO_userguide <- function() {
+  utils::browseURL('docs/UserManual/User_Manual.html')
+}
+
