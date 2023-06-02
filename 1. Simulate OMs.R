5987561e785297894432739880ef96a65d2503ff
Adrian Hordyk <ar.hordyk@gmail.com>
2023-06-02 14:45:59 -0700

    Update 1. Simulate OMs.R

--- 1. Simulate OMs.R
+++ 1. Simulate OMs.R
@@ -3,6 +3,9 @@ library(SWOMSE)
 # ---- Reference OMs ----
 Ref_OMs <- OM_DF %>% filter(Class=='Reference')
 
+if (!dir.exists('Hist_Objects'))
+  dir.create('Hist_Objects')
+
 MOM_Objects <- Ref_OMs$OM.object
 for (i in seq_along(MOM_Objects)) {
   MOM <- get(MOM_Objects[i])
