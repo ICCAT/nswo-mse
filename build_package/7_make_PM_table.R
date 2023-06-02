f4d232ac691703be082a2cc844d46a3ba68588ab
Adrian <ar.hordyk@gmail.com>
2023-06-01 20:14:23 -0700

    Delete 7_make_PM_table.R

--- build_package/7_make_PM_table.R
+++
@@ -1,9 +0,0 @@
-
-
-PM_desc <- read.csv(file.path('dev', 'PM_Description.csv'))
-
-colnames(PM_desc)[4] <- trimws(gsub('\\.', ' ', colnames(PM_desc)[4]))
-
-usethis::use_data(PM_desc, overwrite = TRUE)
-
-
