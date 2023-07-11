62e8a7552d750eedd9b1c8787cb3998023b30452
Adrian <ar.hordyk@gmail.com>
2023-07-11 15:59:30 -0700

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,22 @@ SWO_userguide <- function() {
   utils::browseURL('docs/UserManual/User_Manual.html')
 }
 
+
+#' Trim Data object
+#'
+#' @param Data An object of class `Data`
+#' @param year The year to trim the data to
+#'
+#' @return  An object of class `Data`
+#' @export
+#'
+Trim_Data <- function(Data, year) {
+  Year_ind <- which(Data@Year<=year)
+  Data@Year <- Data@Year[Year_ind]
+  Data@Ind <- Data@Ind[,Year_ind]
+  Data@AddInd <- Data@AddInd[,,Year_ind]
+  Data@Cat <- Data@Cat[,Year_ind]
+  Data@MPrec <- Data@Cat[, max(Year_ind)]
+  Data
+
+}
