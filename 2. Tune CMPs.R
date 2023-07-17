cbf37f3efecb72465ac8225870dd71164f416b25
Adrian <ar.hordyk@gmail.com>
2023-07-17 16:40:13 -0700

    Update 2. Tune CMPs.R

--- 2. Tune CMPs.R
+++ 2. Tune CMPs.R
@@ -15,7 +15,7 @@ Tuning_OMs <- Tuning_OMs$OM.object
 # Tuning Targets
 TuneTargets
 
-MPs <- avail('MP', 'SWOMSE')
+# MPs <- avail('MP', 'SWOMSE')
 
 # --- Scope CMP Performance over Range of Tuning Values ----
 
@@ -23,52 +23,20 @@ MPs <- avail('MP', 'SWOMSE')
 # 2. Loop over MPs and conduct scoping
 
 # ------------------- CE Method -----------------------------------------------
-Tune_MPs <- c('CE', 'CE25')
+# Tune_MPs <- c('CE', 'CE25', 'SPFox', 'SP', 'CI1', 'EA1', 'WA1')
+Tune_MPs <- c('SPS', 'SPS25', 'SPFox25')
 
 for (i in seq_along(Tune_MPs)) {
   MP_name <- Tune_MPs[i]
-  Scope(MP_name, Tuning_OMs, TuneTargets)
-  Plot_Scope(MP_name)
+  tt <- try(Scope(MP_name, Tuning_OMs, TuneTargets))
+ #  Plot_Scope(MP_name)
 }
 
 TuneTarget <- TuneTargets %>% filter(Code=='a')
 
 for (i in seq_along(Tune_MPs)) {
   MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals =c(0.75, 1, 1.25))
-}
-
-TuneTarget <- TuneTargets %>% filter(Code=='b')
-
-for (i in seq_along(Tune_MPs)) {
-  MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals =c(0.9, 0.95, 1))
-}
-
-TuneTarget <- TuneTargets %>% filter(Code=='c')
-
-for (i in seq_along(Tune_MPs)) {
-  MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals =c(0.75, 0.85,0.95))
-}
-
-# --------------------SPS MPs ---------------------------------------------------
-
-
-Tune_MPs <- c('SPS', 'SPS25')
-
-for (i in seq_along(Tune_MPs)) {
-  MP_name <- Tune_MPs[i]
-  Scope(MP_name, Tuning_OMs, TuneTargets)
-  Plot_Scope(MP_name)
-}
-
-
-TuneTarget <- TuneTargets %>% filter(Code=='a')
-
-for (i in seq_along(Tune_MPs)) {
-  MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals =c(1.5, 2, 2.5))
+  tt <- try(Tune(MP_name, Tuning_OMs, TuneTarget))
 }
 
 
@@ -76,57 +44,30 @@ TuneTarget <- TuneTargets %>% filter(Code=='b')
 
 for (i in seq_along(Tune_MPs)) {
   MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals =c(1.6, 1.7, 1.8))
+  tt <- try(Tune(MP_name, Tuning_OMs, TuneTarget))
 }
 
 TuneTarget <- TuneTargets %>% filter(Code=='c')
 
 for (i in seq_along(Tune_MPs)) {
   MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals =c(1.4, 1.5, 1.6))
+  tt <- try(Tune(MP_name, Tuning_OMs, TuneTarget))
 }
 
-
-# --------------------SPFox MPs ---------------------------------------------------
-
-Tune_MPs <- c('SPFox', 'SPFox25')
-
-
-TuneTarget <- TuneTargets %>% filter(Code=='a')
-
-for (i in seq_along(Tune_MPs)) {
-  MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals=c(0.75, 1, 1.25))
-}
-
-
-TuneTarget <- TuneTargets %>% filter(Code=='b')
-
-for (i in seq_along(Tune_MPs)) {
-  MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals =c(0.8, 0.85, 1))
-}
-
-TuneTarget <- TuneTargets %>% filter(Code=='c')
-
-for (i in seq_along(Tune_MPs)) {
-  MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals =c(0.7, 0.8, 0.9))
-}
 
 
 
 # --------------------AH MPs ---------------------------------------------------
 
 
-Tune_MPs <- c('EA1', 'WA1')
+Tune_MPs <- c('CI1')
 
 
 TuneTarget <- TuneTargets %>% filter(Code=='a')
 
 for (i in seq_along(Tune_MPs)) {
   MP_name <- Tune_MPs[i]
-  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals=c(0.75, 1, 1.25))
+  Tune(MP_name, Tuning_OMs, TuneTarget, test_vals=c(1.65, 1.75, 1.85))
 }
 
 
