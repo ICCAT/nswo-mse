d1a2758828dd62ef2a98dd43dba36a9ae821449c
Adrian <ar.hordyk@gmail.com>
2023-08-29 16:38:03 -0700

    Update Shiny.R

--- R/Shiny.R
+++ R/Shiny.R
@@ -3,7 +3,7 @@
 #' \code{Shiny} runs the SWO MSE Shiny Application
 #' @references Modified from Deal Attali's code: \url{http://deanattali.com/2015/04/21/r-package-shiny-app/}
 #' @export
-Shiny <- function() {
+Shiny <- function(...) {
   appDir <- system.file("shiny_apps", 'SWOMSE', package = "SWOMSE")
-  shiny::runApp(appDir, display.mode = "normal",launch.browser = TRUE)
+  shiny::runApp(appDir, ...)
 }
