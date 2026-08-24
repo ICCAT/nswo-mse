4fe6dc5c855e05e456d77857e06c653357a2c9f4
AdrianHordyk <ar.hordyk@gmail.com>
2026-08-24 12:31:23 -0700

    Update Process_Results.R

--- MLL Analysis/Process_Results.R
+++ MLL Analysis/Process_Results.R
@@ -24,30 +24,63 @@ DF_List    <- list()
 for (i in seq_along(YearList)) {
 
   DF_List[[i]] <- dplyr::bind_rows(
+
+    PM_Safety(MSE, 0.4, Years = YearList[[i]]) |> Mean() |>
+      dplyr::mutate(Ratio = Value/Value[MP=='MCC11'],
+                    Period = names(YearList)[i],
+                    Model  = 'Base Case',
+                    PM     = 'Safety') |>
+      dplyr::select(-Stock),
+
     PM_Status(MSE, Years = YearList[[i]]) |> Mean() |>
       dplyr::mutate(Ratio = Value/Value[MP=='MCC11'],
                     Period = names(YearList)[i],
                     Model  = 'Base Case',
-                    PM     = 'Status') ,
+                    PM     = 'Status') |>
+      dplyr::select(-Stock),
 
 
     PM_Yield(MSE, Years = YearList[[i]]) |> Mean() |>
       dplyr::mutate(Ratio = Value/Value[MP=='MCC11'],
                     Period = names(YearList)[i],
                     Model  = 'Base Case',
-                    PM     = 'Yield') ,
+                    PM     = 'TAC') |>
+      dplyr::select(-Stock),
+
+    PM_Landings(MSE, Years = YearList[[i]]) |> Mean() |>
+      dplyr::mutate(Ratio = Value/Value[MP=='MCC11'],
+                    Period = names(YearList)[i],
+                    Model  = 'Base Case',
+                    PM     = 'Landings') |>
+      dplyr::select(-Stock),
+
+    PM_Safety(MSE_Lorenzen, 0.4, Years = YearList[[i]]) |> Mean() |>
+      dplyr::mutate(Ratio = Value/Value[MP=='MCC11'],
+                    Period = names(YearList)[i],
+                    Model  = 'Lorenzen',
+                    PM     = 'Safety') |>
+      dplyr::select(-Stock),
 
     PM_Status(MSE_Lorenzen, Years = YearList[[i]]) |> Mean() |>
       dplyr::mutate(Ratio = Value/Value[MP=='MCC11'],
                     Period = names(YearList)[i],
                     Model  = 'Lorenzen',
-                    PM     = 'Status') ,
+                    PM     = 'Status') |>
+      dplyr::select(-Stock),
 
     PM_Yield(MSE_Lorenzen, Years = YearList[[i]]) |> Mean() |>
       dplyr::mutate(Ratio = Value/Value[MP=='MCC11'],
                     Period = names(YearList)[i],
                     Model  = 'Lorenzen',
-                    PM     = 'Yield')
+                    PM     = 'TAC') |>
+      dplyr::select(-Stock) ,
+
+    PM_Landings(MSE_Lorenzen, Years = YearList[[i]]) |> Mean() |>
+      dplyr::mutate(Ratio = Value/Value[MP=='MCC11'],
+                    Period = names(YearList)[i],
+                    Model  = 'Lorenzen',
+                    PM     = 'Landings') |>
+      dplyr::select(-Stock)
   )
 
 }
@@ -55,14 +88,16 @@ for (i in seq_along(YearList)) {
 
 DF <- dplyr::bind_rows(DF_List)
 
-MSEtool::Save(DF, 'MLL Analysis/DF.rds')
+MSEtool::Save(DF, 'MLL Analysis/DF.rds', overwrite = TRUE)
+
+ DF |> dplyr::filter(Model == 'Base Case', MP == 'MCC11_FR') |>
+  dplyr::select(PM, Period, Ratio) |>
+  tidyr::pivot_wider(names_from = PM, values_from = Ratio)
 
 
-DF |> dplyr::filter(Model == 'Base Case') |> dplyr::arrange(MP)
-
-DF |> dplyr::arrange(Model) |>
-  dplyr::filter(MP == 'MCC11_FR') |>
-  dplyr::select(PM,Period, Model, Ratio) |> print(n=30)
+ DF |> dplyr::filter(Model != 'Base Case', MP == 'MCC11_FR') |>
+   dplyr::select(PM, Period, Ratio) |>
+   tidyr::pivot_wider(names_from = PM, values_from = Ratio)
 
 p1 <- PlotBiomass(MSE,
              Years = 2025:2054,
@@ -137,7 +172,7 @@ Removals <- Removals(MSE, byFleet = FALSE) |>
 df <- dplyr::bind_rows(SB, Removals, Landings)
 df$Variable <- factor(df$Variable, unique(df$Variable), ordered = TRUE)
 
-ggplot(df, aes(x=Year)) +
+p1 <- ggplot(df, aes(x=Year)) +
   facet_wrap(~Variable) +
   geom_line(aes(y=Mean)) +
   geom_hline(yintercept = 1, linetype = 2) +
@@ -145,4 +180,57 @@ ggplot(df, aes(x=Year)) +
   labs(y = 'Relative Change (FR/SQ)') +
   theme_bw()
 
-ggsave('MLL Analysis/RelDiffernceL.png', width = 8, height = 3)
+ggsave('MLL Analysis/RelDiffernceL.png', p1, width = 8, height = 3)
+
+
+
+SB <- SBiomass(MSE_Lorenzen) |>
+  dplyr::filter(Year >= 2025, Stock == 'Female') |>
+  dplyr::left_join(OM_number, by = 'Sim') |>
+  tidyr::pivot_wider(names_from = MP, values_from = Value) |>
+  dplyr::mutate(Rel = MCC11_FR/MCC11, Variable = 'SBiomass') |>
+  dplyr::group_by(Year, Variable) |>
+  dplyr::summarise(Mean = median(Rel),
+                   Lower = quantile(Rel, 0.05),
+                   Upper = quantile(Rel, 0.95))
+
+Landings <- Landings(MSE_Lorenzen, byFleet = FALSE) |>
+  dplyr::filter(Year >= 2025) |>
+  dplyr::group_by(Sim, Year, MP) |>
+  dplyr::summarise(Value = sum(Value)) |>
+  tidyr::pivot_wider(names_from = MP, values_from = Value) |>
+  dplyr::mutate(Rel = MCC11_FR/MCC11, Variable = 'Landings') |>
+  dplyr::group_by(Year, Variable) |>
+  dplyr::summarise(Mean = median(Rel),
+                   Lower = quantile(Rel, 0.05),
+                   Upper = quantile(Rel, 0.95))
+
+Removals <- Removals(MSE_Lorenzen, byFleet = FALSE) |>
+  dplyr::filter(Year >= 2025) |>
+  dplyr::group_by(Sim, Year, MP) |>
+  dplyr::summarise(Value = sum(Value)) |>
+  tidyr::pivot_wider(names_from = MP, values_from = Value) |>
+  dplyr::mutate(Rel = MCC11_FR/MCC11, Variable = 'TAC') |>
+  dplyr::group_by(Year, Variable) |>
+  dplyr::summarise(Mean = median(Rel),
+                   Lower = quantile(Rel, 0.05),
+                   Upper = quantile(Rel, 0.95))
+
+df <- dplyr::bind_rows(SB, Removals, Landings)
+df$Variable <- factor(df$Variable, unique(df$Variable), ordered = TRUE)
+
+p2 <- ggplot(df, aes(x=Year)) +
+  facet_wrap(~Variable) +
+  geom_line(aes(y=Mean)) +
+  geom_hline(yintercept = 1, linetype = 2) +
+  geom_ribbon(aes(ymin = Lower, ymax = Upper), alpha = 0.4) +
+  labs(y = 'Relative Change (FR/SQ)') +
+  theme_bw()
+
+ggsave('MLL Analysis/RelDiffernce_Lorenzen.png', p2, width = 8, height = 3)
+
+
+patchwork::wrap_plots(p1, p2, ncol = 1) +
+  patchwork::plot_annotation(tag_levels = 'A', tag_suffix = ')')
+
+ggsave('MLL Analysis/RelDiffernce.png', width = 8, height = 5)
